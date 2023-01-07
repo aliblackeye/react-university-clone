10bd62b0f38a0465e7a702febb868e56f7a50aff
Berkay ÇAĞLAR <berkay_caglar123@hotmail.com>
2023-01-07 22:48:47 +0300

    Update Header.tsx

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -40,29 +40,10 @@ const Header = () => {
           </div>
         </div>
       </header>
-      <header className="header-main">
-        <div className="container flex items-center justify-between">
-          <div className="header-brand">
-            <img
-              src="https://www.medipol.edu.tr/themes/custom/medipol/logo-en.svg"
-              alt="brand-logo"
-            />
-          </div>
-          <nav className="header-navbar  text-primary text-lg font-semibold">
-            <ul className="nav-items flex gap-10">
-              {navbarElements.map((el, key) => (
-                <li key={key} className="nav-item">
-                  <a className="nav-link" href="#">
-                    {el.name}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </div>
-      </header>
-      <div className="online-services-menu bg-primary text-white font-regular w-[188px] h-[257px] flex flex-col justify-evenly border-t-2 border-white rounded-b">
-        {itemText.map((item, key) => (
+      <br></br>
+      <br></br>
+      <div className="online-services-menu bg-primary text-white font-regular w-[188px] h-[257px] flex flex-col justify-evenly border-t-2 border-white rounded-b mx-auto">
+        {itemText.map((item) => (
           <>
             <div key={key} className="pl-6 flex items-center gap-2">
               <>

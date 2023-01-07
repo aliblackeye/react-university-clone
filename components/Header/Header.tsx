e822fc7cd611a4c4e5c9a0cea65bba0de169a587
aliblackeye <ali.blackeye@hotmail.com>
2023-01-07 19:14:16 +0300

    header component update

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,22 +1,34 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 
+const navbarElements = [
+  "ACADEMICS",
+  "ABOUT MEDIPOL",
+  "PROSPECTIVE STUDENTS",
+  "ACTIVE STUDENTS",
+  "RESEARCH",
+];
+
 const Header = () => {
   return (
     <header>
       <div className="header container flex items-center justify-between">
-        <div className="header-brand  py-5">
+        <div className="header-brand">
           <img
             src="https://www.medipol.edu.tr/themes/custom/medipol/logo-en.svg"
             alt="brand-logo"
           />
         </div>
-        <nav className="header-navbar flex gap-10">
-          <span>ACADEMICS</span>
-          <span>ABOUT MEDIPOL</span>
-          <span>PROSPECTIVE STUDENTS</span>
-          <span>ACTIVE STUDENTS</span>
-          <span>RESEARCH</span>
+        <nav className="header-navbar  text-primary text-lg font-semibold">
+          <ul className="nav-items flex gap-10">
+            {navbarElements.map((el, key) => (
+              <li key={key} className="nav-item">
+                <a className="nav-link" href="#">
+                  {el}
+                </a>
+              </li>
+            ))}
+          </ul>
         </nav>
       </div>
     </header>

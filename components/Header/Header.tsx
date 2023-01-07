67a7f6f246bb499ae22718e4d30ad3db89a12d0e
aliblackeye <ali.blackeye@hotmail.com>
2023-01-07 18:07:02 +0300

    header component created

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <div className="header container flex items-center justify-between">
+        <div className="header-brand  py-5">
+          <img
+            src="https://www.medipol.edu.tr/themes/custom/medipol/logo-en.svg"
+            alt="brand-logo"
+          />
+        </div>
+        <nav className="header-navbar flex gap-10">
+          <span>ACADEMICS</span>
+          <span>ABOUT MEDIPOL</span>
+          <span>PROSPECTIVE STUDENTS</span>
+          <span>ACTIVE STUDENTS</span>
+          <span>RESEARCH</span>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;

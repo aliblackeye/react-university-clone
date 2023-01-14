a41aaaa37121d411d3ae4bfb460a89be704052bd
aliblackeye <ali.blackeye@hotmail.com>
2023-01-14 14:58:32 +0300

    link key

--- components/Header/_partials/OnlineServicesMenu.tsx
+++ components/Header/_partials/OnlineServicesMenu.tsx
@@ -18,13 +18,12 @@ export const OnlineServicesMenu = () => {
       <div className=" bg-primary text-white font-regular inline-flex flex-col shadow-2xl rounded-b w-[220px]">
         {itemText.map((item, key) => {
           if (item.divider) {
-            return <div className="divider"></div>;
+            return <div key={key} className="divider"></div>;
           } else {
             return (
-              <Link href="#" style={{ textDecoration: "none" }}>
+              <Link key={key} href="#" style={{ textDecoration: "none" }}>
                 <div className="p-8 flex items-center space-x-2 transition-all hover:bg-white hover:text-primary hover:font-semibold cursor-pointer">
                   {item.icon}
-
                   <span>{item.text}</span>
                 </div>
               </Link>

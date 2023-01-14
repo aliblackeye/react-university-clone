7122ffe0fa1da2747ba47ac021eaf7cf790f0342
Berkay ÇAĞLAR <berkay_caglar123@hotmail.com>
2023-01-14 14:55:18 +0300

    Update OnlineServicesMenu.tsx

--- components/Header/_partials/OnlineServicesMenu.tsx
+++ components/Header/_partials/OnlineServicesMenu.tsx
@@ -14,7 +14,7 @@ const itemText = [
 
 export const OnlineServicesMenu = () => {
   return (
-    <div className="online-services-menu absolute z-10 right-[-50px] top-10 flex justify-center">
+    <div className="online-services-menu absolute z-10 right-[-50px] top-[56px] flex justify-center">
       <div className=" bg-primary text-white font-regular inline-flex flex-col shadow-2xl rounded-b w-[220px]">
         {itemText.map((item, key) => {
           if (item.divider) {

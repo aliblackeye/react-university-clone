cdf9073589c91d03939351c2d9bad2e862f13b9c
Berkay ÇAĞLAR <berkay_caglar123@hotmail.com>
2023-01-14 14:43:47 +0300

    Updated! Top Menu+ Online Services Menu+

--- components/Header/_partials/OnlineServicesMenu.tsx
+++ components/Header/_partials/OnlineServicesMenu.tsx
@@ -1,27 +1,36 @@
 // Icons
+import Link from "next/link";
 import { FiBookOpen } from "react-icons/fi";
 import { HiOutlineMail } from "react-icons/hi";
 import { TfiPencil } from "react-icons/tfi";
 // Datas
 const itemText = [
   { text: "MEBIS", icon: <FiBookOpen /> },
+  { divider: true },
   { text: "Webmail (Outlook)", icon: <HiOutlineMail /> },
+  { divider: true },
   { text: "Online İşlemler", icon: <TfiPencil /> },
 ];
 
 export const OnlineServicesMenu = () => {
   return (
-    <div>
-      <div className="online-services-menu bg-primary text-white font-regular inline-flex flex-col border-t-2 border-white rounded-b mx-auto">
-        {itemText.map((item, key) => (
-          <div key={key} className=" hover:bg-white">
-            <div className="p-4 flex items-center gap-2">
-              <span>{item.icon}</span>
-              <span>{item.text}</span>
-            </div>
-            <hr></hr>
-          </div>
-        ))}
+    <div className="online-services-menu absolute z-10 right-[-50px] top-10 flex justify-center">
+      <div className=" bg-primary text-white font-regular inline-flex flex-col shadow-2xl rounded-b w-[220px]">
+        {itemText.map((item, key) => {
+          if (item.divider) {
+            return <div className="divider"></div>;
+          } else {
+            return (
+              <Link href="#" style={{ textDecoration: "none" }}>
+                <div className="p-8 flex items-center space-x-2 transition-all hover:bg-white hover:text-primary hover:font-semibold cursor-pointer">
+                  {item.icon}
+
+                  <span>{item.text}</span>
+                </div>
+              </Link>
+            );
+          }
+        })}
       </div>
     </div>
   );

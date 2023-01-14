5174c9a3861b8fec37b003dbaf680e0bc8a1d6fe
Berkay ÇAĞLAR <berkay_caglar123@hotmail.com>
2023-01-14 12:52:27 +0300

    Updated Online Services Menu

--- components/Header/_partials/OnlineServicesMenu.tsx
+++ components/Header/_partials/OnlineServicesMenu.tsx
@@ -11,18 +11,18 @@ const itemText = [
 
 export const OnlineServicesMenu = () => {
   return (
-    <div className="online-services-menu bg-primary text-white font-regular w-[188px] h-[257px] flex flex-col justify-evenly border-t-2 border-white rounded-b mx-auto">
-      {itemText.map((item, key) => (
-        <>
-          <div key={key} className="pl-6 flex items-center gap-2 ">
-            <>
-              {item.icon}
+    <div>
+      <div className="online-services-menu bg-primary text-white font-regular inline-flex flex-col border-t-2 border-white rounded-b mx-auto">
+        {itemText.map((item, key) => (
+          <div key={key} className=" hover:bg-white">
+            <div className="p-4 flex items-center gap-2">
+              <span>{item.icon}</span>
               <span>{item.text}</span>
-            </>
+            </div>
+            <hr></hr>
           </div>
-          <hr></hr>
-        </>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
